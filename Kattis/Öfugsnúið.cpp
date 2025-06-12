4c98b732607ade2f9e4bedde7502ae78249ede02
abel2319 <kpohinto.abel@gmail.com>
2025-06-12 22:16:33 +0200

    feat(new-problem):Ofugsnuid

--- Kattis/Öfugsnúið.cpp
+++ Kattis/Öfugsnúið.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
-#include <std/set>
+#include <vector>
 
 int main() {
-	int n;
+	int n, input;
+	std::vector<int> vec;
+
+	std::cin >> n;
+	
+	for (int i = 0; i < n; ++i) {
+		std::cin >> input;
+		vec.push_back(input);
+	}
+
+	for (auto it = vec.rbegin(); it != vec.rend(); ++it) {
+		std::cout << *it;
+		if (it != vec.rend() - 1)
+			std::cout << std::endl;
+    	}
 
 	return 0;
 }
